9f6c69b91620fbf1511c6f29a9bf712f89753a8f
quguiyou <quguiyou1076@qq.com>
2023-08-13 19:12:52 +0800

    接收来自客户端的请求

--- httpd.c
+++ httpd.c
@@ -1,9 +1,17 @@
 #include "httpd.h"
 
 #include <arpa/inet.h>
+#include <stdio.h>
 #include <string.h>
 #include <sys/socket.h>
 #include <unistd.h>
+
+#define httpd_log(fmt, ...)     \
+  {                             \
+    printf("httpd:");           \
+    printf(fmt, ##__VA_ARGS__); \
+    printf("\n");               \
+  }
 
 void httpd_init(void) {}
 
@@ -11,6 +19,7 @@ int httpd_start(uint16_t port) {
   int server_socket = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
 
   if (server_socket < 0) {
+    httpd_log("create socket failed.");
     goto start_error;
   }
 
@@ -22,11 +31,24 @@ int httpd_start(uint16_t port) {
 
   if (bind(server_socket, (const struct sockaddr *)&server_addr,
            sizeof(server_addr)) < 0) {
+    httpd_log("bind error");
     goto start_error;
   }
 
   if (listen(server_socket, HTTPD_QUEUE_NR) < 0) {
+    httpd_log("listen error");
     goto start_error;
+  }
+
+  httpd_log("server is running, port: %d", port);
+  for (;;) {
+    struct sockaddr_in addr;
+    socklen_t len = sizeof(addr);
+    int sock = accept(server_socket, (struct sockaddr *)&addr, &len);
+    if (sock < 0) {
+      httpd_log("accept error");
+      goto start_error;
+    }
   }
 
   close(server_socket);
